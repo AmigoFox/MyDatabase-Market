ff330aecbaa1c3629de807cc30c670d93da03947
AmigoFox <prostoggwpandre@gmail.com>
2025-12-15 08:57:06 -0900

    MAUI create ui main menu
    MAUI create ui main menu

---
+++ cross-platform application/app/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app.Pages")]
---
+++ cross-platform application/app/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace app
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCalculatorClicked(object? sender, EventArgs e)
+        {
+            await DisplayAlert("Уведомление", "Уже скоро", "ок");
+        }
+    }
+}
